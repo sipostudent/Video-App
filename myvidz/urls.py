6e56ed5fb6a2d3ce9143038cb633a9d81bb8cf3e
Sipo Charles <sipocharles@yahoo.co.uk>
2020-11-11 14:20:42 +0000

    Authentication Templates

--- myvidz/urls.py
+++ myvidz/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin, auth
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from halls import views
 from django.conf.urls.static import static
