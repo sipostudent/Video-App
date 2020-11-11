9427a2bdaf0d3204aa78fa9792aa75f5d82e4840
Sipo Charles <sipocharles@yahoo.co.uk>
2020-11-11 13:17:25 +0000

    Authentication Views

--- myvidz/urls.py
+++ myvidz/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin, auth
 from django.urls import path
 from halls import views
 from django.conf.urls.static import static
@@ -22,5 +22,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    # AUTH
+    path('signup', views.SignUp.as_view(), name='signup'),
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
