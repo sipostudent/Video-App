b86d7a47a5a1c341a86c4248fd8c19b9da5b6faf
Sipo Charles <sipocharles@yahoo.co.uk>
2020-11-12 01:01:44 +0000

    Class Based Views

--- halls/views.py
+++ halls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from .models import Hall
 
 
 def home(request):
@@ -12,3 +13,10 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
+
+
+class CreateHall(generic.CreateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/create_hall.html'
+    success_url = reverse_lazy('home')
--- myvidz/urls.py
+++ myvidz/urls.py
@@ -27,5 +27,10 @@ urlpatterns = [
     path('signup', views.SignUp.as_view(), name='signup'),
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    # Hall
+    path('halloffame/create', views.CreateHall.as_view(), name='create_hall'),
+    # path('halloffame/<int:pk>', views.DetailHall.as_view(), name='detail_hall'),
+    # path('halloffame/<int:pk>/update', views.UpdateHall.as_view(), name='update_hall'),
+    # path('halloffame/<int:pk>/delete', views.DeleteHall.as_view(), name='delete_hall'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
